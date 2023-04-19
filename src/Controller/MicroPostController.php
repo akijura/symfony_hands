3a1169e25ca85fb7a710ab59addc98364b4321ec
akijura <akki.jura96@gmail.com>
2023-04-19 12:06:25 -0400

    update add

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -7,14 +7,17 @@ use App\Repository\MicroPostRepository;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class MicroPostController extends AbstractController
 {
     #[Route('/micro/post', name: 'app_micro_post')]
-    public function index(MicroPostRepository $posts): JsonResponse
+    public function index(MicroPostRepository $posts): Response
     {
-        //dd($posts->findAll());
+        // dd($posts->findAll());
 
         //adding to database
         // $microPost = new MicroPost();
@@ -24,13 +27,47 @@ class MicroPostController extends AbstractController
         // $posts->save($microPost,true);
 
         //update record
-        $microPost = $posts->find(4);
-        $microPost->setTitle('Welcome in general');
-        $posts->save($microPost,true);
+        // $microPost = $posts->find(4);
+        // $microPost->setTitle('Welcome in general');
+        // $posts->save($microPost,true);
         
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MicroPostController.php',
+        return $this->render('micro_post/index.html.twig',[
+            'posts' => $posts->findAll()
         ]);
+    }
+    #[Route('/micro/post/{post}', name: 'app_micro_post_show')]
+    public function showOne(MicroPost $post): Response
+    {
+        return $this->render('micro_post/show.html.twig',[
+            'post' => $post
+        ]);
+    }
+    
+    #[Route('/micro/post/add', name: 'app_micro_post_add',priority:2)]
+    public function add(Request $request, MicroPostRepository $posts): Response
+    {
+        $microPost = new MicroPost();
+        $form = $this->createFormBuilder($microPost)
+        ->add('title')
+        ->add('text')
+        ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+            $posts->save($post,true);
+            
+            // add a flash
+            $this->addFlash('success','Your micro post has been added');
+            return $this->redirectToRoute('app_micro_post');
+            // redirect to other page
+        }
+        return $this->render('micro_post/add.html.twig',
+            [
+                'form' => $form
+            ]
+        );
     }
 }
