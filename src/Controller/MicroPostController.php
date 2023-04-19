a947c31c02ca69553f11b109b3a39ea2abdbd10f
akijura <akki.jura96@gmail.com>
2023-04-19 12:43:12 -0400

    changed

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -7,10 +7,8 @@ use App\Form\MicroPostType;
 use App\Repository\MicroPostRepository;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 
 class MicroPostController extends AbstractController
@@ -18,20 +16,6 @@ class MicroPostController extends AbstractController
     #[Route('/micro/post', name: 'app_micro_post')]
     public function index(MicroPostRepository $posts): Response
     {
-        // dd($posts->findAll());
-
-        //adding to database
-        // $microPost = new MicroPost();
-        // $microPost->setTitle('It comes from controller');
-        // $microPost->setText('Here akki');
-        // $microPost->setCreated(new DateTime());
-        // $posts->save($microPost,true);
-
-        //update record
-        // $microPost = $posts->find(4);
-        // $microPost->setTitle('Welcome in general');
-        // $posts->save($microPost,true);
-        
         return $this->render('micro_post/index.html.twig',[
             'posts' => $posts->findAll()
         ]);
@@ -55,8 +39,8 @@ class MicroPostController extends AbstractController
             $posts->save($post,true);
             // add a flash
             $this->addFlash('success','Your micro post has been added');
-            return $this->redirectToRoute('app_micro_post');
             // redirect to other page
+            return $this->redirectToRoute('app_micro_post');
         }
         return $this->render('micro_post/add.html.twig',
             [
@@ -64,7 +48,7 @@ class MicroPostController extends AbstractController
             ]
         );
     }
-    
+
     #[Route('/micro/post/{post}/edit', name: 'app_micro_post_edit')]
     public function edit(MicroPost $post,Request $request,MicroPostRepository $posts): Response
     {
@@ -73,11 +57,10 @@ class MicroPostController extends AbstractController
         if($form->isSubmitted() && $form->isValid()) {
             $post = $form->getData();
             $posts->save($post,true);
-            
             // add a flash
             $this->addFlash('success','Your micro post has been updated');
-            return $this->redirectToRoute('app_micro_post');
             // redirect to other page
+            return $this->redirectToRoute('app_micro_post');
         }
         return $this->render('micro_post/edit.html.twig',
             [
