998a49b413486ffba7fe029f817d621f76637130
akbarjon <juraev.a@hamkorbank.uz>
2023-05-02 06:40:29 -0400

    query optimizing

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -6,6 +6,7 @@ use App\Entity\Comment;
 use App\Entity\MicroPost;
 use App\Form\CommentType;
 use App\Form\MicroPostType;
+use App\Entity\User;
 use App\Repository\CommentRepository;
 use App\Repository\MicroPostRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -20,7 +21,27 @@ class MicroPostController extends AbstractController
     public function index(MicroPostRepository $posts): Response
     {
         return $this->render('micro_post/index.html.twig',[
-            'posts' => $posts->findAll()
+            'posts' => $posts->findAllWithComments()
+        ]);
+    }
+    #[Route('/micro/post/top-liked', name: 'app_micro_post_topliked')]
+    public function topLiked(MicroPostRepository $posts): Response
+    {
+
+        return $this->render('micro_post/top_liked.html.twig',[
+            'posts' => $posts->findAllWithMinLikes(2)
+        ]);
+    }
+    #[Route('/micro/post/follows', name: 'app_micro_post_follows')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function follows(MicroPostRepository $posts): Response
+    {
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+        return $this->render('micro_post/follows.html.twig',[
+            'posts' => $posts->findAllByAuthors(
+                $currentUser->getFollows()
+            )
         ]);
     }
     #[Route('/micro/post/{post}', name: 'app_micro_post_show')]
